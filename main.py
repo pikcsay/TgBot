d2895fc41e2fdba401eb66322b3437d5d89df3b8
pikcsay <pikcsay@gmail.com>
2021-04-13 18:56:55 +0300

    Add files via upload
    add wiki on english

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import requests
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
+import wikipedia as wiki
 
 TOKEN = ''
+wiki.set_lang("en")
+
+
+def wiki_info(update, context):
+    words = ' '.join(context.args)
+    info = wiki.summary("{}".format(words), chars=4096)
+    update.message.reply_text('Вот что мне удалось найти по запросу {}:\n{}'.format(words, info))
 
 
 def start(update, context):
@@ -26,6 +35,16 @@ def quests():
     pass
 
 
+#def translate(update, context):
+#    params = {
+#        "key": KEY,
+#        "text": ' '.join(context.args),
+#        "lang": 'ru-en'
+#    }
+#    response = requests.get(URL, params=params)
+#    update.message.reply_text(response.json()['text'])
+
+
 class Vocabulary:
     pass
 
@@ -45,6 +64,8 @@ if __name__ == '__main__':
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    #dp.add_handler(CommandHandler("translate", translate))
+    dp.add_handler(CommandHandler("wiki", wiki_info))
 
     updater.start_polling()
 
