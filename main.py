4e8823133a632c297b5559e49be0c8359b7eae98
pikcsay <pikcsay@gmail.com>
2021-04-13 19:18:52 +0300

    Add files via upload
    add news us

--- main.py
+++ main.py
@@ -2,15 +2,33 @@ import requests
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
 import wikipedia as wiki
+from newsapi import NewsApiClient
 
 TOKEN = ''
 wiki.set_lang("en")
+newsapi = NewsApiClient(api_key='bc1e0bf5348f4d1fb1a88bf6d4305ab9')
 
 
 def wiki_info(update, context):
     words = ' '.join(context.args)
     info = wiki.summary("{}".format(words), chars=4096)
     update.message.reply_text('Вот что мне удалось найти по запросу {}:\n{}'.format(words, info))
+
+
+def get_news(update, context):
+    if context.args[0] in ('technology', 'science', 'entertainment', 'general'):
+        category = context.args[0]
+    else:
+        category = 'general'
+    k = 7
+    top_headlines = newsapi.get_top_headlines(category=category, language='en', country='us')
+    if top_headlines['status'] == 'ok':
+        if top_headlines['totalResults'] < 7:
+            k = top_headlines['totalResults']
+        news = top_headlines['articles']
+        print(top_headlines)
+        top_headlines = ['• {}: {}'.format(i['source']['name'], i['title']) for i in news[:k]]
+        update.message.reply_text('\n'.join(top_headlines))
 
 
 def start(update, context):
@@ -35,7 +53,7 @@ def quests():
     pass
 
 
-#def translate(update, context):
+# def translate(update, context):
 #    params = {
 #        "key": KEY,
 #        "text": ' '.join(context.args),
@@ -64,8 +82,9 @@ if __name__ == '__main__':
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    #dp.add_handler(CommandHandler("translate", translate))
+    # dp.add_handler(CommandHandler("translate", translate))
     dp.add_handler(CommandHandler("wiki", wiki_info))
+    dp.add_handler(CommandHandler('news', get_news))
 
     updater.start_polling()
 
